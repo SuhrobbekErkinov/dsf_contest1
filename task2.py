64a4caa40b245a9e78008f121dafd495fbebe505
SuhrobbekErkinov <s.erkinov@newuu.uz>
2025-02-13 15:57:50 +0500

    firstcommit

--- task2.py
+++ task2.py
@@ -1,12 +1,12 @@
 def typeBasedTransformer(**kwargs):
     result = {}
     for key, value in kwargs.items():
-        if isinstance(value, (int, float)):
+        if isinstance(value, bool):
+            result[key] = not value
+        elif isinstance(value, (int, float)):
             result[key] = value ** 2
         elif isinstance(value, str):
             result[key] = value[::-1]
-        elif isinstance(value, bool):
-            result[key] = not value
         elif isinstance(value, (list, tuple)):
             result[key] = value[::-1]
         elif isinstance(value, dict):
